4b47105d4789ebce78a981c79a623686cd8da0df
Stoica Marius <busterxl95@gmail.com>
2019-11-10 15:53:38 +0200

    Canny Updated

--- image_processing_tool-master/Application/ImageProcessingAlgorithms/Canny.py
+++ image_processing_tool-master/Application/ImageProcessingAlgorithms/Canny.py
@@ -3,6 +3,9 @@ from Application.Utils.InputDecorators import InputDialog
 from Application.Utils.OutputDecorators import OutputDialog
 from scipy import ndimage
 import numpy as np
+import queue
+from skimage import data, filters
+import cv2
 
 @RegisterAlgorithm("Canny","Tema4")
 @InputDialog(tMin=float,tMax=float)
@@ -11,54 +14,191 @@ import numpy as np
 def canny(image,tMin,tMax):
     fx=np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
     fy=np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
-    #image=ndimage.gaussian_filter(image,1)
-    image = filtruGauss(image, 1)
-
-    maskafx=ndimage.filters.convolve(image,fx,mode='constant',cval=0)
-    maskafy=ndimage.filters.convolve(image, fy, mode='constant', cval=0)
+    image=ndimage.gaussian_filter(image,1)
+    #image = filtruGauss(image, 1)
+    #print(fx)
+    #print(fy)
+    image=np.array(image,dtype='float')
+    maskafx=ndimage.filters.convolve(image,fx,mode='constant',cval=0.0)
+    maskafy=ndimage.filters.convolve(image,fy, mode='constant', cval=0.0)
 
     gradient=np.hypot(maskafx,maskafy)
     grade=np.arctan2(maskafy,maskafx)
+    ndimage.filters.maximum_filter
     directia=detDirection(grade)
-    imagineSubtiata=subtiere(gradient,directia).astype('uint8')
-    imagineSubtiata=np.where(gradient>tMax,255,imagineSubtiata)
-    imagineSubtiata=np.where(gradient<tMin,0,imagineSubtiata)
+    imagineSubtiata=subtiere(gradient,directia)
+    imagineSubtiata = np.where(imagineSubtiata >= tMax, 255, imagineSubtiata)
+    imagineSubtiata = np.where(imagineSubtiata < tMin, 0, imagineSubtiata)
+    sus_jos=np.array(imagineSubtiata).astype('uint8')
+    stanga_dreapta=np.array(imagineSubtiata).astype('uint8')
+    dreapta_stanga=np.array(imagineSubtiata).astype('uint8')
+    jos_sus=np.array(imagineSubtiata).astype('uint8')
+    sus_jos=parcurgereSJ(sus_jos,tMin,tMax)
+    stanga_dreapta=parcurgereSD(stanga_dreapta,tMin,tMax)
+    dreapta_stanga=parcurgereDS(dreapta_stanga,tMin,tMax)
+    jos_sus=parcurgereJS(jos_sus,tMin,tMax)
+    imagine=sus_jos+stanga_dreapta+dreapta_stanga+jos_sus
+
     return {
-        'processedImage': imagineSubtiata
+        'processedImage': imagine.astype('uint8')
     }
 
+def parcurgereSJ(imagine,tMin,tMax):
+    for i in range(imagine.shape[0] - 1):
+        for j in range(imagine.shape[1] - 1):
+            if (tMin < imagine[i, j] < tMax):
+                if (imagine[i - 1, j + 1] == 255)  or\
+                        (imagine[i - 1, j - 1] == 255) or\
+                        (imagine[i - 1, j] == 255) or\
+                        (imagine[i, j - 1] == 255) or\
+                        (imagine[i, j + 1] == 255) or\
+                        (imagine[i + 1, j - 1] == 255) or\
+                        (imagine[i + 1, j] == 255) or\
+                        (imagine[i + 1, j + 1] == 255):
+                    imagine[i, j] = 255
+                else:
+                    imagine[i, j] = 0
+    return imagine
+
+def parcurgereSD(imagine,tMin,tMax):
+    for i in range(imagine.shape[0] - 1,0,-1):
+        for j in range(imagine.shape[1] - 1):
+            if (tMin < imagine[i, j] < tMax):
+                if (imagine[i - 1, j + 1] == 255)  or\
+                        (imagine[i - 1, j - 1] == 255) or\
+                        (imagine[i - 1, j] == 255) or\
+                        (imagine[i, j - 1] == 255) or\
+                        (imagine[i, j + 1] == 255) or\
+                        (imagine[i + 1, j - 1] == 255) or\
+                        (imagine[i + 1, j] == 255) or\
+                        (imagine[i + 1, j + 1] == 255):
+                    imagine[i, j] = 255
+                else:
+                    imagine[i, j] = 0
+    return imagine
+
+def parcurgereDS(imagine,tMin,tMax):
+    for i in range(imagine.shape[0] - 1):
+        for j in range(imagine.shape[1] - 1,0,-1):
+            if (tMin < imagine[i, j] < tMax):
+                if (imagine[i - 1, j + 1] == 255)  or\
+                        (imagine[i - 1, j - 1] == 255) or\
+                        (imagine[i - 1, j] == 255) or\
+                        (imagine[i, j - 1] == 255) or\
+                        (imagine[i, j + 1] == 255) or\
+                        (imagine[i + 1, j - 1] == 255) or\
+                        (imagine[i + 1, j] == 255) or\
+                        (imagine[i + 1, j + 1] == 255):
+                    imagine[i, j] = 255
+                else:
+                    imagine[i, j] = 0
+    return imagine
+
+def parcurgereJS(imagine,tMin,tMax):
+    for i in range(imagine.shape[0] - 1,0,-1):
+        for j in range(imagine.shape[1] - 1,0,-1):
+            if (tMin < imagine[i, j] < tMax):
+                if (imagine[i - 1, j + 1] == 255)  or\
+                        (imagine[i - 1, j - 1] == 255) or\
+                        (imagine[i - 1, j] == 255) or\
+                        (imagine[i, j - 1] == 255) or\
+                        (imagine[i, j + 1] == 255) or\
+                        (imagine[i + 1, j - 1] == 255) or\
+                        (imagine[i + 1, j] == 255) or\
+                        (imagine[i + 1, j + 1] == 255):
+                    imagine[i, j] = 255
+                else:
+                    imagine[i, j] = 0
+    return imagine
+
+def hister(imagineSubtiata,tMax):
+    Q=queue.Queue()
+    Q.put([0,0])
+    Visited=[]
+    while not Q.empty():
+        flag=False
+        i,j=Q.get()
+        if (imagineSubtiata[i - 1, j + 1] > tMax and not (i,j) in Visited):
+            imagineSubtiata[i, j] = 255
+            flag=True
+        elif (imagineSubtiata[i - 1, j - 1] > tMax and not (i,j) in Visited):
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif (imagineSubtiata[i - 1, j] > tMax and not (i,j) in Visited):
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif (imagineSubtiata[i, j - 1] > tMax and not (i,j) in Visited):
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif(imagineSubtiata[i, j + 1] > tMax and not (i,j) in Visited) :
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif(imagineSubtiata[i + 1, j - 1] > tMax and not (i,j) in Visited) :
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif(imagineSubtiata[i + 1, j] > tMax and not (i,j) in Visited) :
+            imagineSubtiata[i, j] = 255
+            flag = True
+        elif(imagineSubtiata[i + 1, j + 1] > tMax and not (i,j) in Visited):
+            imagineSubtiata[i, j] = 255
+            flag = True
+        else:
+            imagineSubtiata[i, j] = 0
+        if(flag==True):
+            if(i+1<imagineSubtiata.shape[0] and j+1<imagineSubtiata.shape[1]):
+                Q.put([i + 1, j + 1])
+                Q.put([i + 1, j])
+                Q.put([i, j + 1])
+            if (i + 1 < imagineSubtiata.shape[0] and j - 1 >=0 ):
+                Q.put([i + 1, j - 1])
+                Q.put([i, j - 1])
+            if (i - 1 >=0 and j - 1 >= 0):
+                Q.put([i - 1, j])
+                Q.put([i - 1, j - 1])
+                Q.put([i - 1, j + 1])
+            Visited.append((i,j))
+    return  imagineSubtiata
 
 def subtiere(gradient,directia):
     conturImage=np.zeros((gradient.shape[0],gradient.shape[1]))
-    vecinSpate=255
-    vecinFata=255
-    for i in range(gradient.shape[0]):
-        for j in range (gradient.shape[1]):
+    maskaOrizontala=np.ones(5).reshape((5,1))
+    maskaOrizontala=np.ones(5).reshape((1,5))
+    maskaDiagonalaDreapta=np.array([[],[],[],[],[]])
+    for i in range(gradient.shape[0]-1):
+        for j in range (gradient.shape[1]-1):
+            #vecinSpate = 255
+            #vecinFata = 255
+            #orizontal
             if (directia[i,j]==1):
-                vecinFata=gradient[i+1,j]
-                vecinSpate=gradient[i-1,j]
+                vecinFata=gradient[i,j-1]
+                vecinSpate=gradient[i,j+1]
+            #digonala dreapta
             elif(directia[i,j]==2):
-                vecinFata=gradient[i-1,j+1]
-                vecinSpate=gradient[i+1,j-1]
-            elif(directia[i,j]==3):
                 vecinFata=gradient[i+1,j+1]
                 vecinSpate=gradient[i-1,j-1]
+            #verticala
+            elif(directia[i,j]==3):
+                vecinFata=gradient[i+1,j]
+                vecinSpate=gradient[i-1,j]
+            #digonala stanga
             elif(directia[i,j]==4):
-                vecinFata=gradient[i,j+1]
-                vecinSpate=gradient[i,j-1]
+                vecinFata=gradient[i-1,j+1]
+                vecinSpate=gradient[i+1,j-1]
             if(gradient[i,j]>vecinSpate and gradient[i,j]>vecinFata):
-                conturImage[i,j]=255
+                conturImage[i,j]=gradient[i,j]
             else:
                 conturImage[i,j]=0
     return conturImage
 
 def detDirection(degree):
-    degree=(degree*180)/np.pi
-    horizontal=np.where(np.logical_or(-22.5<degree,degree<22.5),1,0)
-    diagonalRight=np.where(np.logical_or(-65.5<degree,degree<-22.5),2,0)
-    diagonalLeft=np.where(np.logical_or(22.5<degree,degree<65.5),3,0)
-    vertical=np.where(np.logical_or(degree>-65.5,degree<65.5),4,0)
-    result=horizontal+diagonalLeft+diagonalRight+vertical
+    degree=degree*180/np.pi
+    degree[degree<0]+=180
+    horizontal=np.where(np.logical_or(np.logical_and(0<=degree,degree<22.5),np.logical_and(157.5<=degree,degree<=180.5)),1,0)
+    diagonalRight=np.where(np.logical_and(22.5<=degree,degree<67.5),2,0)
+    diagonalLeft=np.where(np.logical_and(67.5<=degree,degree<112.5),4,0)
+    vertical=np.where(np.logical_and(degree>=112.5,degree<157.5),3,0)
+    result=np.add(np.add(np.add(horizontal,diagonalLeft),diagonalRight),vertical)
+    print(result.max(),result.min())
     return result
 
 
